e5681c58290d5bb0466cb769d002fa725f8c8e2b
Jia-Yi Jian <jyjian@illinois.edu>
2022-09-30 13:53:11 -0700

    Added animations and sounds

--- game.js
+++ game.js
@@ -3,9 +3,12 @@ var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 3);
+    var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-}
 
-$("randomChosenColour").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);;
+    $("#" + "randomChosenColour").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
