ca8b51c93f7488e3563bebce25c5bd38fb8754c7
Jia-Yi Jian <jyjian@illinois.edu>
2022-09-30 12:36:19 -0700

    Added random selected color function

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-alert("check connection");
+var gamePattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 3);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+}
+
+$("randomChosenColour").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);;
