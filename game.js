812b8d0234219e796455306cb71ce6c107cd1ec2
Jia-Yi Jian <jyjian@illinois.edu>
2022-09-30 17:21:16 -0700

    added animaiton to user click

--- game.js
+++ game.js
@@ -9,13 +9,27 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    $("#" + "randomChosenColour").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
 }
 
 $(".btn").click(function() {
-  var userChosenColour = $(this).sttr("id");
+  var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed")
+  }, 100)
+};
